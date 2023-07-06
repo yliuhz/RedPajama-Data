77fd49d19e22ced1d857034f62152a5ad2f904a5
yliuhz <yliuhz@zju.edu.cn>
2023-07-06 21:42:13 +0800

    Modify script

--- data_prep/cc/cc_net/run.sh
+++ data_prep/cc/cc_net/run.sh
@@ -7,5 +7,4 @@ rm test_efficiency/mined -r
 rm test_efficiency/mined_split -r
 
 
-python -u  -m cc_net --config config/test_efficiency.json 
-2>&1 | tee logs/first_3_pipelines.log
+python -u  -m cc_net --config config/test_efficiency.json 2>&1 | tee logs/first_3_pipelines.log
